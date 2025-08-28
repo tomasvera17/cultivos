638ac7d4be23e373e8015dee2651fff3e5d2a6cf
tomasvera17 <tomasvera17@gmail.com>
2025-08-28 15:10:27 -0400

    cambios clase 28-08

--- cultivos/views.py
+++ cultivos/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 
 def listar_cultivos(request):
-    datos = {
+    cultivos_originales = [
+        {'nombre': 'Tomate', 'tipo': 'fruto'},
+        {'nombre': 'Lechuga', 'tipo': 'hoja'},
+        {'nombre': 'Albahaca', 'tipo': 'hoja'},
+        {'nombre': 'Zanahoria', 'tipo': 'raiz'},
+        {'nombre': 'Cilantro', 'tipo': 'hoja'},
+        {'nombre': 'Pimiento', 'tipo': 'fruto'},
+    ]
+    termino = request.GET.get('buscar', '')
+    tipo = request.GET.get('tipo', '')
+    cultivo_filtrados = cultivos_originales
+    if termino:
+        cultivo_filtrados = [c for c in cultivo_filtrados if termino.lower() in c['nombre'].lower()]
+    if tipo:
+        cultivo_filtrados = [c for c in cultivo_filtrados if c['tipo'] == tipo]
+    
+    contexto = {
         'titulo': 'Mi huerto',
-        'mensaje': 'Esta es la lista simulada de cultivos',
-        'cultivos': ['Tomates', 'Papas', 'Lechuga', 'Zanahoria']
+        'mensaje': 'Filtra los cultivos por nombre y tipo',
+        'cultivos': cultivo_filtrados,
     }
-    return render(request, 'cultivos/lista_cultivos.html', datos)
+    return render(request, 'cultivos/lista_cultivos.html', contexto)
